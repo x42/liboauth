a5ac0291d2002fc09aed7cd6ea679da1a4c38e67
Robin Gareus <robin@gareus.org>
2010-05-21 22:27:33 +0200

    replaced xmalloc with custom MIT-licensed version.

--- src/xmalloc.h
+++ src/xmalloc.h
@@ -1,30 +1,16 @@
 #ifndef _OAUTH_XMALLOC_H
 #define _OAUTH_XMALLOC_H      1 
 
-#if HAVE_CONFIG_H
-# include <config.h>
-#endif
-
-#ifndef USE_GPL
-
-#define xmalloc malloc
-#define xcalloc calloc
-#define xrealloc realloc
-#define xstrdup strdup
-
-#else 
-
 /* Prototypes for functions defined in xmalloc.c  */
-void *xmalloc (size_t n);
-void *xcalloc (size_t n, size_t s);
-void *xrealloc (void *p, size_t n);
-char *xstrdup (const char *p);
+void *xmalloc (size_t size);
+void *xcalloc (size_t nmemb, size_t size);
+void *xrealloc (void *ptr, size_t size);
+char *xstrdup (const char *s);
 
-/* POSIX prototypes - avoid compiler warnings with '-posix' */
+/* POSIX - avoid compiler warnings with '-posix -std=c99 -pedantic' */
 int strncasecmp(const char *s1, const char *s2, size_t n);
 int snprintf(char *str, size_t size, const char *format, ...);
 FILE *popen(const char *command, const char *type);
 int pclose(FILE *stream);
-#endif // USE_GPL
 
 #endif
