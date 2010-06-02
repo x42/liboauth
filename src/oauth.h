dda2ef89ee58e548394bf47b3f9df3578a455d06
Robin Gareus <robin@gareus.org>
2010-06-02 17:52:48 +0200

    fixed test-code build-system (CFLAGS & LDADD)

--- src/oauth.h
+++ src/oauth.h
@@ -29,10 +29,10 @@
 
 #ifndef DOXYGEN_IGNORE
 // liboauth version
-#define LIBOAUTH_VERSION "0.8.6"
+#define LIBOAUTH_VERSION "0.8.7"
 #define LIBOAUTH_VERSION_MAJOR  0
 #define LIBOAUTH_VERSION_MINOR  8
-#define LIBOAUTH_VERSION_MICRO  6
+#define LIBOAUTH_VERSION_MICRO  7
 
 //interface revision number
 //http://www.gnu.org/software/libtool/manual/html_node/Updating-version-info.html
