9bd2494be1d8e483a6d2bb6d3eabebd83fd43f31
Robin Gareus <robin@gareus.org>
2011-01-18 17:46:21 +0100

    bumped version number to v0.9.3

--- src/oauth.h
+++ src/oauth.h
@@ -29,10 +29,10 @@
 
 #ifndef DOXYGEN_IGNORE
 // liboauth version
-#define LIBOAUTH_VERSION "0.9.2"
+#define LIBOAUTH_VERSION "0.9.3"
 #define LIBOAUTH_VERSION_MAJOR  0
 #define LIBOAUTH_VERSION_MINOR  9
-#define LIBOAUTH_VERSION_MICRO  2
+#define LIBOAUTH_VERSION_MICRO  3
 
 //interface revision number
 //http://www.gnu.org/software/libtool/manual/html_node/Updating-version-info.html
