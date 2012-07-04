5b8c487aa24aef90962c230337c021896dbe180d
Robin Gareus <robin@gareus.org>
2012-07-04 11:49:17 +0200

    version v0.9.7

--- src/oauth.h
+++ src/oauth.h
@@ -29,15 +29,15 @@
 
 #ifndef DOXYGEN_IGNORE
 // liboauth version
-#define LIBOAUTH_VERSION "0.9.6"
+#define LIBOAUTH_VERSION "0.9.7"
 #define LIBOAUTH_VERSION_MAJOR  0
 #define LIBOAUTH_VERSION_MINOR  9
-#define LIBOAUTH_VERSION_MICRO  6
+#define LIBOAUTH_VERSION_MICRO  7
 
 //interface revision number
 //http://www.gnu.org/software/libtool/manual/html_node/Updating-version-info.html
 #define LIBOAUTH_CUR  8
-#define LIBOAUTH_REV  3
+#define LIBOAUTH_REV  4
 #define LIBOAUTH_AGE  8
 #endif
 
